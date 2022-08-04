e5b5348184fda84a8601e359563c5c0a364a88c8
toto <antoniolabate19@gmail.com>
2022-08-04 22:17:40 +0200

    category set to entertainment

--- command.sh
+++ command.sh
@@ -3,5 +3,5 @@ python3 upload_video.py --file="$1" \
                         --title="$titleDescr" \
                         --description="$titleDescr" \
                         --keywords="tiktok,reddit,videos,funny" \
-                        --category="" \
+                        --category="24" \ # 24: entertainment
                         --privacyStatus="public"
