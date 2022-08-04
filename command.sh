614bc2e5669c849313d8a0d77e269aba14b43c2b
toto <antoniolabate19@gmail.com>
2022-08-04 17:54:51 +0200

    new codebase from google docs

---
+++ command.sh
@@ -0,0 +1,7 @@
+titleDescr="Tiktok video compilation #$2"
+python3 upload_video.py --file="$1" \
+                        --title="$titleDescr" \
+                        --description="$titleDescr" \
+                        --keywords="tiktok,reddit,videos,funny" \
+                        --category="" \
+                        --privacyStatus="public"
