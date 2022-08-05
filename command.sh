99b5618face95c1e4b7c230f9d6b31337a382286
toto <antoniolabate19@gmail.com>
2022-08-05 16:36:17 +0200

    fixed upload initializing script

--- command.sh
+++ command.sh
@@ -1,7 +1,7 @@
-titleDescr="Tiktok video compilation #$2"
+titleDescr="$2"
 python3 upload_video.py --file="$1" \
                         --title="$titleDescr" \
                         --description="$titleDescr" \
                         --keywords="tiktok,reddit,videos,funny" \
-                        --category="24" \ # 24: entertainment
+                        --category="24" \
                         --privacyStatus="public"
