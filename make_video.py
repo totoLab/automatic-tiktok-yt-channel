32492ccfe554728dea18260e307992c430180c70
toto <antoniolabate19@gmail.com>
2023-08-01 18:26:39 +0200

    removed backticks that ffmpeg doesn't require

--- make_video.py
+++ make_video.py
@@ -72,15 +72,7 @@ def blurring(file_list_path):
             titles.write(title)
 
     with open(file_list_path, "w") as file_list:
-        for filename in os.listdir(DOWNLOAD_DIR):
-            output_path = os.path.join(BLUR_DIR, filename)
-            if filename.endswith(".mp4") and filename not in os.listdir(BLUR_DIR):
-                input_path = os.path.join(DOWNLOAD_DIR, filename)
-                subprocess.run([
-                    "ffmpeg", "-y", "-i", input_path,
-                    "-lavfi", "[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16",
-                    "-vb", "800K", output_path
-                ])
+                    file_list.write(f"file {os.path.abspath(output_path)}\n")
 
             file_list.write(f"file `{os.path.abspath(output_path)}`\n")
     print("Applied necessary blurring.")
